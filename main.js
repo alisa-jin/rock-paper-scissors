19599ce67ea92e278ecfd5694911356f6890935b
Alisa Jin <alisaj831@gmail.com>
2023-04-25 20:17:54 -0700

    Add UI to allow button choice of rock, paper, or scissors

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const resultsDiv = document.getElementById('results')
+
 // create a function that returns rock, paper, or scissors
 //Math.random() a number 1 - 3: 1 = rock , 2 = paper, 3 = scissors --> is making a object annoying?
 
@@ -7,7 +9,7 @@ function getComputerChoice() {
     1: 'paper',
     2: 'scissors',
   }
-  let key = Math.floor(Math.random() * 3)
+  const key = Math.floor(Math.random() * 3)
   console.log(resultObj[key])
   return resultObj[key]
 }
@@ -16,56 +18,53 @@ function getComputerChoice() {
 // takes two parameters - (playerSelection, computerSelection)
 function playRound(playerSelection, computerSelection) {
   // make arguments case sesitive for player selection --> .toLowerCase()
-  playerSelection = playerSelection.toLowerCase()
-
-  //test cases for each scenario
-
+  console.log('player selection: ' + playerSelection)
+  console.log('computer selection: ' + computerSelection)
   if (playerSelection === 'rock') {
-    switch (computerSelection) {
-      case 'rock':
-        return 'Draw! Rock and Rock are the same.'
-
-      case 'paper':
-        return 'You Lose! Paper beats Rock.'
-
-      case 'scissors':
-        return 'You Win! Scissors beat Paper.'
-    }
+    if (computerSelection === 'rock') return 'Draw! Rock and Rock are the same.'
+    if (computerSelection === 'paper') return 'You Lose! Paper beats Rock.'
+    if (computerSelection === 'scissors') return 'You Win! Rock beats Scissors'
   } else if (playerSelection === 'paper') {
-    switch (computerSelection) {
-      case 'rock':
-        return 'You Win! Scissors beat Paper.'
-
-      case 'paper':
-        return 'Draw! Paper and Paper are the same.'
-
-      case 'scissors':
-        return 'You Lose! Scissors beats Paper.'
-    }
+    if (computerSelection === 'rock') return 'You Win! Paper beats Rock.'
+    if (computerSelection === 'paper')
+      return 'Draw! Paper and Paper are the same.'
+    if (computerSelection === 'scissors')
+      return 'You Lose! Scissors beats Paper.'
   } else if (playerSelection === 'scissors') {
-    switch (computerSelection) {
-      case 'rock':
-        return 'You Lose! Rock beats Scissors.'
-
-      case 'paper':
-        return 'You Win! Scissors beats Paper.'
-
-      case 'scissors':
-        return 'Draw! Scissors and Scissors are the same.'
-    }
+    if (computerSelection === 'rock') return 'You Lose! Rock beats Scissors.'
+    if (computerSelection === 'paper') return 'You Win! Scissors beats Paper.'
+    if (computerSelection === 'scissors')
+      return 'Draw! Scissors and Scissors are the same.'
   } else {
     return 'Error, wrong input. Please enter: rock, paper, or scissors'
   }
 }
 
-function game() {
-  for (let i = 0; i < 5; i++) {
-    let playerSelection = prompt('Choose rock, paper, or scissors: ')
-    console.log(playerSelection)
-    let computerSelection = getComputerChoice()
-    console.log(computerSelection)
-    console.log(playRound(playerSelection, computerSelection))
-  }
+function displayResults(result) {
+  resultsDiv.innerHTML = result
 }
 
-game()
+const rockBtn = document.getElementById('rock-btn')
+rockBtn.onclick = () => {
+  displayResults(playRound('rock', getComputerChoice()))
+}
+const paperBtn = document.getElementById('paper-btn')
+paperBtn.onclick = () => {
+  displayResults(playRound('paper', getComputerChoice()))
+}
+const scissorsBtn = document.getElementById('scissors-btn')
+scissorsBtn.onclick = () => {
+  displayResults(playRound('scissors', getComputerChoice()))
+}
+
+// function game() {
+//   for (let i = 0; i < 5; i++) {
+//     let playerSelection = prompt('Choose rock, paper, or scissors: ')
+//     console.log(playerSelection)
+//     let computerSelection = getComputerChoice()
+//     console.log(computerSelection)
+//     console.log(playRound(playerSelection, computerSelection))
+//   }
+// }
+
+// game()
